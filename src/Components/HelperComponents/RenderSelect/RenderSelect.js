a9ccf135ecc90d19af228d3aacbc6a0197e093a9
micola10174 <micola.cascenco@gmail.com>
2020-11-28 21:56:11 +0200

    styled select-react

--- src/Components/HelperComponents/RenderSelect/RenderSelect.js
+++ src/Components/HelperComponents/RenderSelect/RenderSelect.js
@@ -2,10 +2,33 @@ import React from 'react';
 import Select from 'react-select';
 import './RenderSelect.scss';
 
+
+const customStyles = {
+    menu: (provided, state) => ({
+        ...provided,
+        maxWidth: 320,
+      }),
+    
+    control: () => ({
+        // none of react-select's styles are passed to <Control />
+        border: '1px solid hsl(0,0%,80%);',
+        maxWidth: 320,
+        display: 'flex',
+        marginBottom: 20
+      }),
+  }
+
 const RenderSelect = ({field, options, form}) => {
     return (
         <div className="wrapper-select">
-            <Select name={field.name} options={options} value={options ? options.find(option => option.value === field.value) : ''} onChange={(option) => form.setFieldValue(field.name, option.value)}/>
+            <Select 
+                width="320px"
+                name={field.name} 
+                options={options} 
+                value={options ? options.find(option => option.value === field.value) : ''} 
+                onChange={(option) => form.setFieldValue(field.name, option.value)}
+                styles={customStyles}
+            />
         </div>
     );
 }
